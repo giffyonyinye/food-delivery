0d4e9b282d5738368595a721a5074e72dc7f9058
giffyonyinye <giffyonyinye@gmail.com>
2023-07-23 14:09:40 +0100

    styling fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,19 @@
 function openModal() {
-  var modal = document.getElementById("modalBackground");
+  const modal = document.getElementById("modalBackground");
   modal.style.display = "flex";
 }
 
 function closeModal() {
-  var modal = document.getElementById("modalBackground");
+  const modal = document.getElementById("modalBackground");
   modal.style.display = "none";
 }
 
 function openCleaningModal() {
-  var modal = document.getElementById("cleaningModalBackground");
+  const modal = document.getElementById("cleaningModalBackground");
   modal.style.display = "flex";
 }
 
 function closeCleaningModal() {
-  var modal = document.getElementById("cleaningModalBackground");
+  const modal = document.getElementById("cleaningModalBackground");
   modal.style.display = "none";
 }
